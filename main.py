5853f8b79f2f3de98bda289018972a2b90561384
MusaTamzid05 <tamzid101@gmail.com>
2024-10-16 17:07:03 +0600

    add scheduler

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from lib.crawler import Crawler
+import datetime as dt
+import schedule
+import time
+
+
+def run_crawler():
+    print("Running crawler")
+    crawler = Crawler()
+    crawler.run(debug=True)
 
 
 if __name__ == "__main__":
-    crawler = Crawler()
-    crawler.run(debug=True)
+    run_crawler()
+    schedule.every(5).hours.do(run_crawler)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
